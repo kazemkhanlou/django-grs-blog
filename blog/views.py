25f8eae36c61c46343ca09514ea0cb6cd00f4227
kazemkhanlou <kazemkhanlou@gmail.com>
2025-02-18 19:39:41 +0330

    Added the HTML template for the homepage of the site.

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+
+def post_list(request):
+    return render(request, 'blog/post_list.html', {})
+    # return HttpResponse("Here will go the homepage content.")
